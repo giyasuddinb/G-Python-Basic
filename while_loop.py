7359bcbfdd15ebb811716071ff61f6d7763ff292
MD Giyas Uddin B <giasbmd1995@gmail.com>
2025-09-09 07:54:55 +0600

    while loop practice

---
+++ while_loop.py
@@ -0,0 +1,33 @@
+# count = 0
+# while count<10:
+#     print(count, 'While Loop')
+#     count +=1
+
+
+# data = 'Python'
+# index = 0
+# while index<len(data):
+#     print(data[index])
+#     index +=1
+
+# string word find
+data = "I love python"
+data = data.split()
+index =0
+# while index<len(data):
+#     print(data[index])
+#     index+=1
+
+
+
+# Total Number Sum use while loop
+num = [2, 3, 4,5,6,7,8,300]
+total = 0
+index = 0
+while (index<len(num)):
+    total = total + num[index]
+    print('index num:', index, 'Total Num:', total)
+    index+=1
+
+print('Total Sum Number of',total)
+
